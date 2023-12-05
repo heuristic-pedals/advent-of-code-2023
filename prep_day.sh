e51f97f0784bef308d47e7476da015719ffacc34
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-05 17:41:57 +0000

    feat: added new day utility script

---
+++ prep_day.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# A utility script to create blank inputs for a new AoC day.
+
+# get arguments: -d (day number), -f (name for python file)
+while getopts d:f: flag
+do
+    case "${flag}" in
+        d) day_num=${OPTARG};;
+        f) f_name=${OPTARG};;
+    esac
+done
+
+# handle missing args case
+if [ -z "$day_num" ] || [ -z "$f_name" ]; then
+    echo 'Missing -d or -f' >&2
+    exit 1
+else
+    # create data folder, text files, and empty python file for that day
+    echo "Prepping for day number: $day_num"
+    mkdir data/day_"$day_num"
+    touch data/day_"$day_num"/input.txt
+    touch data/day_"$day_num"/practice_input.txt
+    mkdir advent_of_code_2023/day_"$day_num"
+    touch advent_of_code_2023/day_"$day_num"/"$f_name".py
+fi
