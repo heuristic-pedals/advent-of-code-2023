0b8e3d802ade5f3c8f0e2869649dcc4d6d2fb87c
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-06 18:58:10 +0000

    feat: added day 6 part 2 solution

--- advent_of_code_2023/day_6/boat_race.py
+++ advent_of_code_2023/day_6/boat_race.py
@@ -7,7 +7,7 @@ Source: https://adventofcode.com/2023/day/6
 import re
 
 from collections import defaultdict
-from math import prod
+from math import prod, sqrt, ceil, floor
 
 from advent_of_code_2023.utils import read_text_file
 
@@ -63,6 +63,78 @@ def multirace_winning_button_press_times(lines: list[str]) -> defaultdict(int):
     return broken_records
 
 
+def real_quaradtic_roots(a: int, b: int, c: int) -> tuple[float, float]:
+    """Get real roots of a quadratic equaton.
+
+    Parameters
+    ----------
+    a : int
+        Coefficient a.
+    b : int
+        Coefficient b.
+    c : int
+        Coeficient c.
+
+    Returns
+    -------
+    tuple[float, float]
+        Real roots of quadratice equation for provided a, b, c
+
+    Raises
+    ------
+    ValueError
+        When real roots do not exist.
+
+    """
+    # calculate the discriminant
+    discriminant = b**2 - (4 * a * c)
+
+    # handle case when no real roots exist
+    if not discriminant > 0:
+        raise ValueError(
+            "Discriminant is not positive, therefore solutions are not real or"
+            f" do not exist. Got discriminant = {discriminant}."
+        )
+
+    # get real solutions
+    sol_1 = ((-1 * b) + sqrt(discriminant)) / (2 * a)
+    sol_2 = ((-1 * b) - sqrt(discriminant)) / (2 * a)
+
+    return sol_1, sol_2
+
+
+def singlerace_winning_button_press_times(lines: list[str]) -> int:
+    """Calculate the number of winning button press durations.
+
+    Parameters
+    ----------
+    lines : list[str]
+        Input contain race times and distance record.
+
+    Returns
+    -------
+    int
+        number of possible winning button pressing durations.
+
+    """
+    # split at : and take only the right hand side, repalce spaces and convert
+    race_time = int(lines[0].split(":")[1].replace(" ", ""))
+    record_dist = int(lines[1].split(":")[1].replace(" ", ""))
+
+    # get roots to -t(b)**2 + t(r)t(b) - d(r) = 0
+    # t(b): button press time, t(r): race time, d(r): race distance record
+    solutions = real_quaradtic_roots(-1, race_time, -1 * record_dist)
+
+    # get whole number of milliseonds that are still valid solutions
+    min_button_time = ceil(solutions[0])
+    max_button_time = floor(solutions[1])
+
+    # add one since we need the number of combinations, not the range
+    number_of_cases = max_button_time - min_button_time + 1
+
+    return number_of_cases
+
+
 if __name__ == "__main__":
     # prep input
     lines = read_text_file(INPUT_PATH)
@@ -73,4 +145,11 @@ if __name__ == "__main__":
     print(
         "Part 1: Product of the number of winning pressing combinations is "
         f"{winning_prod}"
+    )
+
+    # part 2 solution
+    num_scenarios = singlerace_winning_button_press_times(lines)
+    print(
+        "Part 2: Number of winning button press duration combinations is "
+        f"{num_scenarios}"
     )
