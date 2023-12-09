0f308c3af6694ad32d12cb5d5f5bb9ef8fc23f90
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-09 18:05:21 +0000

    feat: added day 9 part 2 solution

--- advent_of_code_2023/day_9/mirage_maintenance.py
+++ advent_of_code_2023/day_9/mirage_maintenance.py
@@ -9,17 +9,21 @@ from advent_of_code_2023.utils import read_text_file
 INPUT_FILEPATH = "data/day_9/input.txt"
 
 
-def next_number(line: str) -> int:
-    """Get the next number of the sequence using a 'diff to zero' type method.
+def extrapolate_sequence(line: str, extrapolate_back=False) -> int:
+    """Extrapolate the sequence using a 'diff to zero' type method.
 
     Continually get the difference between consequetive numbers until all the
-    differences are zero. The next number in the sequence is then the sum of
-    all the last differences.
+    differences are zero. Then extrapolate to get the past or future number of
+    the original sequence.
 
     Parameters
     ----------
     line : str
-        input line containing the sequence (space separated ints).
+        Input line containing the sequence (space separated ints).
+    extrapolate_back : bool, optional
+        Used to control whether to calculate the past of future number. By
+        default False, meaning the future number will be extrapolated. Set to
+        True when extrapolating to the past number is desired.
 
     Returns
     -------
@@ -29,7 +33,7 @@ def next_number(line: str) -> int:
     """
     # get the sequence digits and check if they are all zero
     i = 0
-    arrs = [parse_line(line)]
+    arrs = [parse_line(line, extrapolate_back=extrapolate_back)]
     all_diffs = [x == 0 for x in arrs[i]]
 
     # continue to get the difference between consequitive numbers until all the
@@ -64,13 +68,18 @@ def get_diffs(nums: list[int]) -> list[int]:
     return [nums[i + 1] - nums[i] for i in range(0, len(nums) - 1)]
 
 
-def parse_line(line: str) -> list[int]:
+def parse_line(line: str, extrapolate_back=False) -> list[int]:
     """Parse an input line into a list of integers.
+
+    When extrapolated_back is True, the sequence is reversed so that the past
+    number can be extrapolated using the same methods.
 
     Parameters
     ----------
     line : str
         input line
+    extrapolate_back : bool, optional
+        Used to reverse the sequence of numbers when extrapolating backwards.
 
     Returns
     -------
@@ -78,7 +87,10 @@ def parse_line(line: str) -> list[int]:
         input line split at spaces and all digits converts to ints.
 
     """
-    return [int(x) for x in line.split(" ")]
+    nums = [int(x) for x in line.split(" ")]
+    if extrapolate_back:
+        nums.reverse()
+    return nums
 
 
 if __name__ == "__main__":
@@ -86,5 +98,11 @@ if __name__ == "__main__":
     lines = read_text_file(INPUT_FILEPATH)
 
     # part 1 solution
-    sum_next_nums = sum([next_number(line) for line in lines])
+    sum_next_nums = sum([extrapolate_sequence(line) for line in lines])
     print(f"Part 1: Sum of next numbers is {sum_next_nums}")
+
+    # part 2 solution
+    sum_first_nums = sum(
+        [extrapolate_sequence(line, extrapolate_back=True) for line in lines]
+    )
+    print(f"Part 2: Sum of first numbers is {sum_first_nums}")
