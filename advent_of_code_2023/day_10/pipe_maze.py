d1fc251cf38c938879eb09ac22244f0f78f03da9
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-10 19:04:16 +0000

    feat: added day 10 part 2 solution

--- advent_of_code_2023/day_10/pipe_maze.py
+++ advent_of_code_2023/day_10/pipe_maze.py
@@ -5,13 +5,14 @@ Source: https://adventofcode.com/2023/day/10
 """
 
 from math import ceil
+from shapely.geometry import Polygon, Point
 
 from advent_of_code_2023.utils import read_text_file
 
 INPUT_FILEPATH = "data/day_10/input.txt"
 
 
-def max_maze_distance(maze: list[str]) -> int:
+def max_maze_distance(maze: list[str]) -> tuple[int, list[tuple[int, int]]]:
     """Calculate the distance to the furthest point from the starting tile.
 
     Parameters
@@ -21,8 +22,10 @@ def max_maze_distance(maze: list[str]) -> int:
 
     Returns
     -------
-    int
-        further distance from the starting location, in number of tiles.
+    tuple[int, list[tuple[int, int]]]
+        1st element is the further distance from the starting location, in
+        number of tiles. The 2nd element corresponds to the i, j coordinates of
+        the maze route (from start back to start).
 
     """
     # a maze symbol, current direction to next direction lookup
@@ -39,21 +42,24 @@ def max_maze_distance(maze: list[str]) -> int:
     # get starting config
     i, j = find_start(maze)
     direction = get_start_direction(maze, i, j)
+    maze_route = []
 
     num_moves = 0
     while True:
-
+        maze_route.append((i, j))
         # increment the indecies and check if back at the start
         i, j = move_indecies(i, j, direction)
         num_moves += 1
         if maze[i][j] == "S":
+            # append start onto end to complete the polygon
+            maze_route.append(maze_route[0])
             break
 
         # otherwise, translate direction to the next tile
         connector = maze[i][j]
         direction = maze_symbols_lup[connector][direction]
 
-    return ceil(num_moves / 2)
+    return ceil(num_moves / 2), maze_route
 
 
 def print_maze(maze: list[str]) -> None:
@@ -162,10 +168,48 @@ def move_indecies(i: int, j: int, direction: str) -> tuple[int, int]:
     return i, j
 
 
+def count_containing_tiles(maze_route: list[tuple[int, int]]) -> int:
+    """Count the number of tiles within the shape defined by the maze route.
+
+    Parameters
+    ----------
+    maze_route : list[tuple[int, int]]
+        A list of i, j coordinates defining the maze route.
+
+    Returns
+    -------
+    int
+        Number of tiles within the maze route
+
+    """
+    # convert to a polygon
+    poly = Polygon(maze_route)
+
+    # get the bounds, and create a grid within that bounds
+    minx, miny, maxx, maxy = poly.bounds
+    minx, miny, maxx, maxy = int(minx), int(miny), int(maxx), int(maxy)
+    tiles_in_route = [
+        [x, y] for x in range(minx, maxx + 1) for y in range(miny, maxy + 1)
+    ]
+
+    # check if each tile is contained by the polygon and count if so
+    count_within = 0
+    for tile in tiles_in_route:
+        tile_point = Point(tile)
+        if poly.contains(tile_point):
+            count_within += 1
+
+    return count_within
+
+
 if __name__ == "__main__":
     # prep input
     lines = read_text_file(INPUT_FILEPATH)
 
     # part 1 solution
-    distance = max_maze_distance(lines)
+    distance, maze_route = max_maze_distance(lines)
     print(f"Part 1: Distance to travel from the start is {distance}")
+
+    # part 2 solution
+    count_within = count_containing_tiles(maze_route)
+    print(f"Part 2: No. tiles within the maze route is {count_within}")
